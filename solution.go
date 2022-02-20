248e528f87e29f61a1f8bd812ab4bb16f540e86f
Adam Kuzikov <kuzikov.dev@protonmail.com>
2022-02-20 16:58:33 +0600

    solution 1

--- solution.go
+++ solution.go
@@ -1,12 +1,30 @@
 package square
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+import "math"
+
+// Define custom int type to hold sides number.
+type side int
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
+const (
+	circle   side = 0
+	triangle side = 3
+	square   side = 4
+)
+
+// CalcSquare (10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+func CalcSquare(sideLen float64, sidesNum side) float64 {
+	switch sidesNum {
+	case circle:
+		return (math.Pi * sideLen * sideLen)
+	case triangle:
+		return (sideLen * sideLen * math.Sqrt(3) / 4)
+	case square:
+		return (sideLen * sideLen)
+	default:
+		return 0.0
+	}
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 }
---
+++ solution_test.go
@@ -0,0 +1,28 @@
+package square
+
+import "testing"
+
+func TestCalcSquare(t *testing.T) {
+	type variant struct {
+		sideLen float64
+		sideNum int
+		expect  float64
+	}
+
+	tests := []variant{
+		// circle
+		{10, 0, 314.1592653589793},
+		// triangle
+		{4, 3, 6.928203230275509},
+		// square
+		{5, 4, 25},
+	}
+
+	for _, v := range tests {
+		result := CalcSquare(v.sideLen, side(v.sideNum))
+		if result != v.expect {
+			t.Errorf("want:%v, have:%v", v.expect, result)
+		}
+	}
+
+}
